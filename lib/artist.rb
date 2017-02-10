6ceff1f57d0a77854b424c75b6e3f2fd1d1e4b82
David Ordovskiy <david.ordovsky@gmail.com>
2017-02-10 01:46:25 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
