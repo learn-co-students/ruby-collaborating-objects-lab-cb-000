42a5e5844b5c2e85beddf6aeb6363a2d04af8ee5
James Garcia <jgarcia@garciajames.com>
2017-04-19 21:01:46 +0000

    Done with updates

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   @@all = []
 
@@ -11,10 +11,6 @@ class Artist
 
   def self.all
     @@all
-  end
-
-  def songs
-    @songs
   end
 
   def add_song(song)
@@ -28,7 +24,7 @@ class Artist
   end
 
   def self.find_by_name(name)
-    @@all.detect {|artist| artist.name == name}
+    self.all.detect {|artist| artist.name == name}
   end
 
   def self.find_or_create_by_name(name)
@@ -40,7 +36,7 @@ class Artist
   end
 
   def print_songs
-    @songs.each {|song| puts "#{song.name}"}
+    @songs.each {|song| puts song.name}
   end
 
 end
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,20 +4,19 @@ class MP3Importer
 
   def initialize(file_path)
     @path = file_path
-    @files = []
+    # @files = []
   end
 
   def files
-    file_names = Dir.glob("#{path}/*.mp3")
-    @files = file_names.collect do |file|
-      File.basename file
-    end
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+    # file_names = Dir.glob("#{path}/*.mp3")
+    # @files = file_names.collect do |file|
+    #   File.basename file
+    # end
   end
 
   def import
-    self.files.each do|file_name|
-      Song.new_by_filename(file_name)
-    end
+    files.each {|file_name| Song.new_by_filename(file_name)}
   end
 
 end
