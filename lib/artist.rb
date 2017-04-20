c313fcf864a371f021ef13b49995edad1614d164
Gabriel Constantin Staicu <gabriel_staicu@yahoo.com>
2017-04-20 14:06:19 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,5 @@
+#require 'pry'
+
 class Artist
   @@all = []
   attr_accessor :name
@@ -9,12 +11,13 @@ class Artist
   end
 
   def add_song(song)
-    @songs << song
+    @songs << song unless @songs.any? {|s| s.name == song.name}
   end
 
   def self.create_by_name(name)
     artist = self.new(name)
     artist.save
+    artist
   end
 
   def save
@@ -26,11 +29,20 @@ class Artist
   end
 
   def self.find_by_name(name)
-    self.all.detect {|artist| artist.name == name}
+    self.all.find {|artist| artist.name == name}
   end
 
   def self.find_or_create_by_name(name)
     find_by_name(name) ? find_by_name(name) : create_by_name(name)
   end
 
+  def print_songs
+    @songs.each { |song| puts "#{song.name}" }
+  end
+
 end
+
+##for debug
+#artist_1 = Artist.find_or_create_by_name("Drake")
+#binding.pry
+#puts "debug"
