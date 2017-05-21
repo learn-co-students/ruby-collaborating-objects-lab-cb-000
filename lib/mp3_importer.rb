ddba71c0c341f68e12454bfb7137b6e5d6abb207
Alyssa Hogan <alyssabhogan@gmail.com>
2017-05-21 20:55:15 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+class MP3Importer
+
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files ||= Dir["#{path}/*.mp3"].collect do |filename|
+      filename.gsub("#{path}/", "")
+    end
+  end
+
+  def import
+    files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+
+end
