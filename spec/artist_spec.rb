b0cca8075256c69778e45af040b46c2761d29c34
cptbrkfst <dyl@resonategroup.ca>
2017-06-10 14:21:47 -0400

    cleaned initial clone

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,5 +1,5 @@
 describe 'Artist' do
-  let(:artist) {Artist.new('Michael Jackson')}
+  let(:artist) { Artist.new('Michael Jackson') }
 
   describe '#initialize with #name' do
     it 'accepts a name for the artist' do
@@ -16,8 +16,8 @@ describe 'Artist' do
 
   describe '#songs' do
     it 'keeps track of an artist\'s songs' do
-      song_one = Song.new("Rock With You")
-      song_two = Song.new("Smooth Criminal")
+      song_one = Song.new('Rock With You')
+      song_two = Song.new('Smooth Criminal')
       artist.add_song(song_one)
       artist.add_song(song_two)
       expect(artist.songs).to eq([song_one, song_two])
@@ -32,25 +32,26 @@ describe 'Artist' do
   end
 
   describe '.find_or_create_by_name' do
-    it 'finds or creates an artist by name maintaining uniqueness of objects by name property' do
-      artist_1 = Artist.find_or_create_by_name("Michael Jackson")
-      artist_2 = Artist.find_or_create_by_name("Michael Jackson")
+    it 'finds or creates an artist by name maintaining uniqueness
+        of objects by name property' do
+      artist_1 = Artist.find_or_create_by_name('Michael Jackson')
+      artist_2 = Artist.find_or_create_by_name('Michael Jackson')
       expect(artist_1).to eq(artist_2)
     end
 
     it 'Creates new instance of Artist if none exist' do
-      artist_1 = Artist.find_or_create_by_name("Drake")
+      artist_1 = Artist.find_or_create_by_name('Drake')
       expect(artist_1.class).to eq(Artist)
     end
   end
 
   describe '#print_songs' do
     it 'lists all of the artist\'s songs' do
-      dirty_diana = Song.new("Dirty Diana")
-      billie_jean = Song.new("Billie Jean")
+      dirty_diana = Song.new('Dirty Diana')
+      billie_jean = Song.new('Billie Jean')
       artist.add_song(dirty_diana)
       artist.add_song(billie_jean)
-      expect{artist.print_songs}.to output("Dirty Diana\nBillie Jean\n").to_stdout
+      expect{artist.print_songs}.to output('Dirty Diana\nBillie Jean\n').to_stdout
     end
   end
 end
