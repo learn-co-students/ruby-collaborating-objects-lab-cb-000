855b4a928008c3d77f7b2db2a83a43e5ed82841f
Jen Poole <contact.vandepool@gmail.com>
2017-12-17 19:23:54 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,10 +27,31 @@ class Artist
 
   # finds or creates an artist by name maintaining uniqueness of objects by name property
   # Creates new instance of Artist if none exist
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name) # if given artist name is found, return found name; else create artist name
+  end
+
+  # given an artist name, find all songs that belong to that artist
+  def self.find(name)
+    self.all.find { |artist| artist.name == name}
+  end
+
+  # create & save a new artist if artist name does not exist
+  def self.create(name)
+    self.new(name).tap { |artist| artist.save }
   end
 
   # lists all of the artist's songs
   def print_songs
+    songs.each { |song| puts song.name }
   end
 end
+
+
+=begin
+  #tap method: Yields x to the block, and then returns x
+
+  https://apidock.com/ruby/Object/tap
+
+  https://medium.com/aviabird/ruby-tap-that-method-90c8a801fd6a
+=end
--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,19 @@ class Song
   end
 
   # creates a new instance of a song from the file that's passed in
-  def self.new_by_filename
+  def self.new_by_filename(filename)
+    artist = filename.split(' - ')[0]
+    song = filename.split(' - ')[1]
+
+    new_song = self.new(song)
+    new_song.artist_name = artist
+
+    new_song
+  end
+
+  # finds or creates an artist by name
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 end
