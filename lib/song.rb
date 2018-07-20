3b20e1f6d2e3f713bfec15dbc7e6ad22311cd628
Tao <werewaswas@live.com>
2018-07-20 01:04:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
     @name = name
   end
 
-<<<<<<< HEAD
+# <<<<<<< HEAD
   # def self.new_by_filename(filename)
   #   name = filename.split(' - ')[1]
   #   artist = filename.split(' - ')[0]
@@ -25,13 +25,13 @@ class Song
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
-=======
+# =======
   def self.new_by_filename(filename)
     name = filename.split(' - ')[1]
     artist = filename.split(' - ')[0]
 
     song = self.new(name)
     song.artist = Artist.new(artist)
->>>>>>> 3d6956dade336b1fc9bb1b6c6d27dd483124fe7e
+# >>>>>>> 3d6956dade336b1fc9bb1b6c6d27dd483124fe7e
   end
 end
