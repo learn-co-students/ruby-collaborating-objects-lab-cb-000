080eba12d7f238477f10d5140ccf64795f1bacc4
Tao <werewaswas@live.com>
2018-07-20 01:01:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,7 +12,10 @@ class Artist
   def initialize(name)
     @songs = []
     @name = name
+<<<<<<< HEAD
     # @@all << self
+=======
+>>>>>>> 3d6956dade336b1fc9bb1b6c6d27dd483124fe7e
   end
 
   def add_song(song)
@@ -24,16 +27,17 @@ class Artist
     @@all << self
   end
 
-  # def self.find_or_create_by_name(name)
-  #   artist = @@all.select do |artist| artist.name == name end
-  #     # binding.pry
-  #   artist ? artist[0] : self.create(name)
-  #   # binding.pry
-  # end
-
+<<<<<<< HEAD
   def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.create(name)
+    artist = @@all.find do |artist| artist.name == name end
+      # binding.pry
+    artist ? artist[0] : self.create(name)
+    # binding.pry
   end
+  # 
+  # def self.find_or_create_by_name(name)
+  #   self.find(name) ? self.find(name) : self.create(name)
+  # end
 
   def self.find(name)
     self.all.find {|artist| artist.name == name }
@@ -46,4 +50,13 @@ class Artist
   def print_songs
     songs.each {|song| puts song.name}
   end
+=======
+  def self.find_or_create_by_name(name)
+    artist = @@all.select do |artist| artist.name == name end
+      binding.pry
+    artist[0] ? artist : self.new(name)
+  end
+
+
+>>>>>>> 3d6956dade336b1fc9bb1b6c6d27dd483124fe7e
 end
