f9869a4e5120bec9b8cd0b11da2e3eb8d0538908
Tao <werewaswas@live.com>
2018-07-20 02:01:58 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
 
   @@all = []
 
@@ -10,40 +10,55 @@ class Artist
   end
 
   def initialize(name)
-    @songs = []
     @name = name
-    # @@all << self
   end
 
   def add_song(song)
     # song.artist = self
-    @songs << song
+    song.artist = self
+    song.save
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+    # binding.pry
   end
 
   def save
     @@all << self
   end
 
-  # def self.find_or_create_by_name(name)
-  #   artist = @@all.select do |artist| artist.name == name end
-  #     # binding.pry
-  #   artist ? artist[0] : self.create(name)
-  #   # binding.pry
-  # end
-
   def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.create(name)
+    artist = @@all.find do |artist| artist.name == name end
+      #  binding.pry
+    artist ? artist : self.create(name)
+    # binding.pry
   end
+  # 
+  # def self.find_or_create_by_name(name)
+  #   self.find(name) ? self.find(name) : self.create(name)
+  # end
 
   def self.find(name)
     self.all.find {|artist| artist.name == name }
   end
 
   def self.create(name)
-    self.new(name).tap {|artist| artist.save}
+    artist = self.new(name)
+    artist.save
+    # artist = self.new(name).tap {|artist| artist.save}
+    artist
   end
 
   def print_songs
     songs.each {|song| puts song.name}
   end
+
+  # def self.find_or_create_by_name(name)
+  #   artist = @@all.select do |artist| artist.name == name end
+  #     binding.pry
+  #   artist[0] ? artist : self.new(name)
+  # end
+
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,20 @@
 class Song
   attr_accessor :artist, :name
-
+  @@all = []
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
   def initialize(name)
     @name = name
   end
 
+# <<<<<<< HEAD
   # def self.new_by_filename(filename)
   #   name = filename.split(' - ')[1]
   #   artist = filename.split(' - ')[0]
@@ -18,11 +28,24 @@ class Song
     artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
+    new_song.save
     new_song
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
   end
+# =======
+  def self.new_by_filename(filename)
+    name = filename.split(' - ')[1]
+    artist = filename.split(' - ')[0]
+
+    song = self.new(name)
+    song.artist = Artist.find_or_create_by_name(artist)
+    song.save
+    song
+# >>>>>>> 3d6956dade336b1fc9bb1b6c6d27dd483124fe7e
+  end
+  
+  
 end
