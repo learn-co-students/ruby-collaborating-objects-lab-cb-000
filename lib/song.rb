e0f980d86905ad848183950af9c4c4750a2c5dd9
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-23 00:44:54 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+  attr_accessor :name, :artist
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.new_by_filename(filename)
+    artist = filename.split(" - ")[0]
+    song = filename.split(" - ")[1]
+    new_artist = Artist.find_or_create_by_name(artist)
+    new_song = self.new(song)
+    new_song.artist = new_artist
+    new_artist.add_song(new_song)
+    return new_song
+  end
+  
+end
